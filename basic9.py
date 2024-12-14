e0f22938e3cbed770d5ff5b3d53df8820be44bc0
S Suwannapaond <saharat.rus@gmail.com>
2024-12-14 17:44:35 +0700

    update

--- basic9.py
+++ basic9.py
@@ -3,19 +3,21 @@
 
 import cv2
 
-cap = cv2.VideoCapture("video/newyork.mp4")
+cap = cv2.VideoCapture(0)
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+result = cv2.VideoWriter("Output" , fourcc , 20.0 , (640,480))
 
 while (cap.isOpened()):
     check , frame = cap.read() #* รับภาพต่อเนื่องจากกล้อง frame ต่อ frame 
     
     if check == True:
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HLS_FULL)
+        ## x = cv2.cvtColor(frame, cv2.COLOR_BGR2HLS_FULL)
         
         cv2.imshow("Output" , frame)
+        result.write(frame)
         if cv2.waitKey(1) & 0xFF == ord('q'): #* เช็กการกดปุัม เมื่อกดปุ่มนั้นๆ จะปิด
             break        
-    else:
-        break #* เมื่อวิดีโอจบ จะปิดอัตโนมัติ
 
+result.release()
 cap.release()
 cv2.destroyAllWindows()
