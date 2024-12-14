117cc203aca2e7e062649d7714599681c81c5483
S Suwannapaond <saharat.rus@gmail.com>
2024-12-14 17:39:55 +0700

    update

---
+++ basic8.py
@@ -0,0 +1,21 @@
+
+#* เปิด vedio แบบ GrayScale Mode
+
+import cv2
+
+cap = cv2.VideoCapture("video/newyork.mp4")
+
+while (cap.isOpened()):
+    check , frame = cap.read() #* รับภาพต่อเนื่องจากกล้อง frame ต่อ frame 
+    
+    if check == True:
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HLS_FULL)
+        
+        cv2.imshow("Output" , frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'): #* เช็กการกดปุัม เมื่อกดปุ่มนั้นๆ จะปิด
+            break        
+    else:
+        break #* เมื่อวิดีโอจบ จะปิดอัตโนมัติ
+
+cap.release()
+cv2.destroyAllWindows()
